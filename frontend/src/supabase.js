a9a8086ee203730a4109443006b34dd46c3f255c
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2021-05-10 19:29:24 +0530

    Delete supabase.js

--- frontend/src/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://vvkigpuupgryhjvwxkrj.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNDY4NTM1MiwiZXhwIjoxOTMwMjYxMzUyfQ.eyUpG9STqntzDJ0mywuRN_T_8akSXg5MeKh4avXPpIU";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export { supabase };
